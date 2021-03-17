250cc1fe45b17c4066bacac0abbe060c05875880
jconeff <jessicaconeff@gmail.com>
2021-03-17 18:34:43 -0400

    tests

---
+++ src/components/About/__tests__/index.test.js
@@ -0,0 +1,18 @@
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import About from '..';
+
+afterEach(cleanup);
+
+describe('About component', () => {
+    // renders About test
+    it('renders', () => {
+        render(<About />);
+    });
+
+    // test case
+    it('matches snapshot DOM node structure', () => {
+        const { asFragment } = render(<About />);
+        expect(asFragment()).toMatchSnapshot();
+    })
+})
